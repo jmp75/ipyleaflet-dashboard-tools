ac1702635ec607ac2c58600a60733a2e4cdc6edb
J-M <jean-michel.perraud@csiro.au>
2021-05-22 16:56:07 +1000

    Adding default cookie-cutter generated files, SFAICS

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for ipyleaflet_dashboard_tools."""
---
+++ tests/test_ipyleaflet_dashboard_tools.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `ipyleaflet_dashboard_tools` package."""
+
+
+import unittest
+
+from ipyleaflet_dashboard_tools import ipyleaflet_dashboard_tools
+
+
+class TestIpyleaflet_dashboard_tools(unittest.TestCase):
+    """Tests for `ipyleaflet_dashboard_tools` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
