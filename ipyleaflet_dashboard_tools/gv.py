cd54c28f8c29c13a4ab559caa2b1fbf79a991b1d
J-M <jean-michel.perraud@csiro.au>
2022-01-10 12:56:37 +1100

    minor improvement to get_data

--- ipyleaflet_dashboard_tools/gv.py
+++ ipyleaflet_dashboard_tools/gv.py
@@ -2,6 +2,8 @@ from typing import Any, Callable, Dict, Tuple
 from IPython.core.display import display
 import xarray as xr
 
+import matplotlib.pyplot as plt
+
 # from bqplot import Axis, Figure, Lines, LinearScale
 # from bqplot.interacts import IndexSelector
 # from bqplot import pyplot as plt
@@ -34,7 +36,7 @@ class GeoViewer:
     def add_marker_info(self, lat:float, lon:float, code:str):
         self.marker_info[(lat, lon)] = code
     
-    def get_code(self, lat, lon):
+    def get_code(self, lat:float, lon:float):
         return self.marker_info[(lat, lon)]
 
     def data_for_identifier(self, ident):
@@ -61,7 +63,6 @@ class GeoViewer:
         # show trace
         markers = []
         for k in self.marker_info:
-            lat, lon = k
             message = self.create_popup(k)
             marker = Marker(location=k)
             marker.on_click(click_handler)
@@ -76,17 +77,24 @@ class GeoViewer:
         # m.add_control(FullScreenControl())
         return m
 
-    def plot_series(self, out_widget:Output, variable, loc_id):
+    def get_data(self, variable:str, loc_id:str, dim_id:str = None):
         """
         """
-        dim_id = None
         if dim_id is None:
             dim_id = self.key
-        tts = self.x_data[variable].sel({dim_id: loc_id})
+        return self.x_data[variable].sel({dim_id: loc_id})
+
+    def plot_series(self, out_widget:Output, variable: str, loc_id: str, dim_id:str = None):
+        """
+        """
+        tts = self.get_data(variable=variable, loc_id=loc_id, dim_id=dim_id)
         # if using bqplot down the track, see https://github.com/jtpio/voila-gpx-viewer
         out_widget.clear_output()
         with out_widget:
-            display(tts.plot(figsize=(16,8)))
+            _ = tts.plot(figsize=(16,8))
+            # do not use display(blah) which then displays the obnoxious matplotlib.lines.Line2D object at etc.>]
+            ax = plt.gca()
+            ax.set_title(loc_id)
             show_inline_matplotlib_plots()
 
     def mk_click_handler_plot_ts(self, out_widget:Output, variable='q_obs_mm'):
@@ -95,7 +103,6 @@ class GeoViewer:
             ident = self.get_code(xy[0], xy[1])
             self.plot_series(out_widget, variable=variable, loc_id=ident)
         return click_handler_plot_ts
-
 
 
 # If printing a data frame straight to an output widget
