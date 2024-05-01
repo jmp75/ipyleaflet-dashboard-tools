3073385e215d8e56f878515eab4c40c2875d33e3
J-M <jean-michel.perraud@csiro.au>
2024-05-01 13:06:44 +1000

    fix typo with relative import

--- ipyleaflet_dashboard_tools/__init__.py
+++ ipyleaflet_dashboard_tools/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for ipyleaflet dashboard tools."""
 
-from _version import __version__
+from ._version import __version__
 
 __author__ = """Jean-Michel Perraud"""
 __email__ = 'jean-michel.perraud@csiro.au'
